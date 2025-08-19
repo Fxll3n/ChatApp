45f94ef47056d55b1358eee6c59151473343e0ec
Angel Bitsov <angel.bitsov@gmail.com>
2025-08-19 10:50:31 -0500

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Angel Bitsov on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
